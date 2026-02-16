6c5ad7dd362ae07274b8665b86b39d886414d549
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-02-16 21:35:25 +0000

    pomodoro app

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// configuration of duration
-const CONFIG = {
-  durations: {
-    work:       25 * 60,
-    shortBreak:  5 * 60,
-    longBreak:  15 * 60,
-  },
-  longBreakAfter: 4,
-};
+let timeLeft;
+let timerId = null;
+let currentMode = "work"; //short break, long break
+
+const modes = {
+  work: 25 * 60,
+  short: 5 * 60,
+  long: 15 * 60
+}
